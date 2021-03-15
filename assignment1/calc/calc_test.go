018bbbd949f5cf29e50bcdd26fce226330ebe3d5
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2021-03-15 20:23:04 +0200

    Update calc_test.go

--- assignment1/calc/calc_test.go
+++ assignment1/calc/calc_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestAdd(t *testing.T) {
 	expected := 3
-	actual := assignment1.Add(1, 2)
+	actual := calc.Add(1, 2)
 
 	if expected != actual {
 		t.Errorf("actual=%d, expected=%d", actual, expected)
